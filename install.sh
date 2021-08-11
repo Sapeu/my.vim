4a73005d86f55d4ffeb7016cabaab982dbacb74f
Sapeu <sapeu.lee@foxmail.com>
2021-08-11 23:40:32 +0800

    feat(init): update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
-clone(){
-	REPOSRC=$1
-	LOCALREPO=$2
+exists_clone(){
+	remote_path=$1
+	local_path=$2
 
-  # We do it this way so that we can abstract if from just git later on
-	LOCALREPO_VC_DIR=$LOCALREPO/.git
-
-	if [[ ! -d $LOCALREPO_VC_DIR ]]
+	if [[ ! -d $local_path/.git ]]
 	then
-		git clone --depth 1 $REPOSRC $LOCALREPO
+		git clone --depth 1 $remote_path $local_path
 	else
-		cd $LOCALREPO
-		git pull $REPOSRC
+		cd $local_path
+		git pull $remote_path
 	fi
 }
 
-clone https://github.com/yianwillis/vimcdoc ~/.vim/pack/docs/start/vimcdoc
-clone https://github.com/joshdick/onedark.vim ~/.vim/pack/colors/opt/onedark.vim
-clone https://github.com/sheerun/vim-polyglot ~/.vim/pack/plugins/start/vim-polyglot
-clone https://github.com/vim-airline/vim-airline ~/.vim/pack/plugins/start/vim-airline
+exists_clone https://github.com/Sapeu/my.vim.git ~/.vim
+exists_clone https://github.com/yianwillis/vimcdoc ~/.vim/pack/docs/start/vimcdoc
+exists_clone https://github.com/joshdick/onedark.vim ~/.vim/pack/colors/opt/onedark.vim
+exists_clone https://github.com/sheerun/vim-polyglot ~/.vim/pack/plugins/start/vim-polyglot
+exists_clone https://github.com/vim-airline/vim-airline ~/.vim/pack/plugins/start/vim-airline
 
 mkdir -p ~/.vim/autoload/lightline/colorscheme/
 rm ~/.vim/autoload/lightline/colorscheme/onedark.vim
